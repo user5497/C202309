caa4469dd9c815bd5430d161aaf10dd02998c9db
user5497 <nk3457@naver.com>
2023-11-01 01:02:30 +0900

    issue #2

--- challenge/week6/studentGrade.c
+++ challenge/week6/studentGrade.c
@@ -29,25 +29,36 @@ void classifyStudents(int scores[], char targetGrade) {
 	}
 }
 
+// scores들을 더하는 역할을 하는 함수. 
 int sumScores(int scores[]) {
-	int sumScore = 0;
-	for (int i = 1; i < STUDENTS; i++) {
-		sumScore += scores[i];
-	}
-	printf("학생 성적의 총 합은 %d입니다.\n", sumScore);
-}
-
-double averageScores(int scores[]) {
-	double averageScore;
-	averageScore =  sumScore/ STUDENTS;
-	printf("학생 성적의 평균 값은 averageScore입니다.");
-}
-
-void printRanks(int scores[]) {
+	int sum = 0;
 	for (int i = 0; i < STUDENTS; i++) {
-		
+		sum += scores[i]; // sum에 배열 scores의 수를 하나씩 더해 합을 구합니다. 
 	}
-	
+	return sum;
+}
+
+// 성적들의 평균을 구하는 함수. 
+double averageScores(int scores[]) {
+	int sum = sumScores(scores); // 함수 sumScores에서 총합을 가져옵니다.
+	double average = (double)sum / (double)STUDENTS; // 분수 값을 가지기 위해 타입을 double로 직접 지정해줍니다. 
+	return average;
+}
+
+
+// 성적들의 순위를 구하는 함수.
+void printRanks(int scores[]) {
+	int ranks[STUDENTS]; // 순위를 저장해둘 배열 선언. 
+	for (int i = 0; i < STUDENTS; i++) {
+		ranks[i] = 1; // 처음 값의 순위를 1로 설정합니다. 
+		for (int j = 0; j < STUDENTS; j++) {
+			if (scores[j] > scores[i]) {
+				ranks[i]++; // i가 고정되었을 때 j가 돌며 모든 값을 비교하면서, 더 큰 다른 요소가 있으면 순위가 증가합니다. 
+			}
+		}
+		printf("%d학생의 순위는 %d입니다. \n", i+1, ranks[i]);
+	}
+
 }
 
 
@@ -61,9 +72,8 @@ int main() {
 		scanf_s("%d", &scores[i]);
 	}
 
-	char ch = getchar(); // 버퍼 임시 저장 변수, 엔터 지우기 위해서
+	char ch = getchar(); // 엔터를 지우기 위한 버퍼 임시 저장 함수. 
 
-	// char target을 선언합니다. 
 	char target;
 	printf("특정 점수(A,B,C,D,F)를 입력하시오: ");
 	scanf_s("%c", &target, 1);
@@ -72,6 +82,9 @@ int main() {
 
 	int sum = sumScores(scores);
 	double average = averageScores(scores);
+	printf("학생들 점수의 총 합은 %d이고, 평균 값은 %lf입니다.\n", sum, average);
+
+	printRanks(scores);
 
 	return 0;
 }
